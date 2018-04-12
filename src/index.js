4f3475c8c26a2203ceafc1d1c3c4de76c9454d18
Marco DiPlacido <marcodi@microsoft.com>
2018-04-12 17:20:13 -0400

    making the tool a little smarter.  will add more built in strategies.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ var jsdiff = require('diff');
 var fs = require('fs');
 var stringify = require('json-stable-stringify');
 
-var file = args.argv.file;
-var split = args.argv.split || "to deep equal";
-var strategy = args.argv.strategy || "parse";  // or eval
+var strategies = [
+    {
+        splitOn: "to deep equal",
+        formatWith: "parse"
+    }
+];
 
-var shouldClean = args.argv.clean || false;
-var shouldFormat = args.argv.format || false;
+var file = args.argv.file;
+var split = args.argv.split || "auto";
+var strategy = args.argv.strategy || "auto";
+
+var shouldClean = args.argv.clean || true;
+var shouldFormat = args.argv.format || true;
 var logging = args.argv.logging || false;
 
 if (!file || !fs.existsSync(file)) {
-    throw new Error("cannot find left file " + file);
+    throw new Error("cannot find --file " + file);
 }
 
 function log(message) {
@@ -33,24 +40,79 @@ function clean(input) {
         .replace(/\}\)/g, "}");
 }
 
-function format(input) {
+function format(input, createObjectStrategy) {
     if (!shouldFormat) {
         return input;
     }
 
-    log("Format input: " + input);
+    log("");
+    log("Format input: " + input + " format with " + createObjectStrategy);
     log("");
 
-    var obj = strategy === "parse" ? JSON.parse(input) : eval(input);
+    const trySequence = [
+        createObjectStrategy === "parse" ? tryParse : tryEval,
+        createObjectStrategy === "parse" ? tryEval : tryParse
+    ];
+
+    // can't figure out how to express this in a clean functional way, this is basically saying find the
+    // first matching parsed object
+    const obj = trySequence
+        .reduce((prev, curr) => prev || curr(input), null);
+
+    if (!obj) {
+        throw new Error("cannot parse object! turn on logging to see verbose errors eg. --logging");
+    }
+
     return stringify(obj, { space: 4 });
+}
+
+function tryParse(input) {
+    log("in tryParse");
+    try {
+        return JSON.parse(input);
+    } catch (error) {
+        log(error);
+        return null;
+    }
+}
+
+function tryEval(input) {
+    log("in tryEval");
+    try {
+        return eval(input);
+    } catch (error) {
+        log(error);
+        return null;
+    }
 }
 
 var content = fs.readFileSync(file, 'utf8');
 
-var i = content.indexOf(split);
+if (split !== "auto") {
+    strategies = [{
+        splitOn: split,
+        formatWith: strategy
+    }, ...strategies];
+}
 
-var firstRaw = content.substring(0, i);
-var secondRaw = content.substring(i + split.length);
+var config = strategies.map(config => ({
+    strategy: {
+        splitOn: config.splitOn,
+        formatWith: strategy === "auto" ?
+            config.formatWith :
+            strategy
+    },
+    index: content.indexOf(config.splitOn)
+})).find(current => current.index !== -1) || { index: -1, strategy: null };
+
+if (config.index === -1) {
+    throw new Error("cannot find split string!");
+}
+
+log("using Config: " + stringify(config, { space: 4 }));
+
+var firstRaw = content.substring(0, config.index);
+var secondRaw = content.substring(config.index + config.strategy.splitOn.length);
 
 log("first input raw: ");
 log(firstRaw);
@@ -60,8 +122,8 @@ log("");
 log("second input raw: ");
 log(secondRaw);
 
-var first = format(clean(firstRaw));
-var second = format(clean(secondRaw));
+var first = format(clean(firstRaw), config.strategy.formatWith);
+var second = format(clean(secondRaw), config.strategy.formatWith);
 
 var diff = jsdiff.diffLines(first, second);
 
