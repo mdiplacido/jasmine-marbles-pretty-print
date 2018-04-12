e247be0cab5a225db4d98a3bc11ceb31a5a4fe9f
Marco DiPlacido <marcodi@microsoft.com>
2018-04-11 23:04:15 -0400

    adding more clean expressions, adding eval strategy and logging

--- src/index.js
+++ src/index.js
@@ -6,18 +6,31 @@ var stringify = require('json-stable-stringify');
 
 var file = args.argv.file;
 var split = args.argv.split || "to deep equal";
+var strategy = args.argv.strategy || "parse";  // or eval
+
 var shouldClean = args.argv.clean || false;
 var shouldFormat = args.argv.format || false;
+var logging = args.argv.logging || false;
 
 if (!file || !fs.existsSync(file)) {
     throw new Error("cannot find left file " + file);
+}
+
+function log(message) {
+    if (logging) {
+        console.log(message);
+    }
 }
 
 function clean(input) {
     if (!shouldClean) {
         return input;
     }
-    return input.replace(/\"\:\"\[/g, "\":[").replace(/\]\"/g, "]");
+    return input
+        .replace(/\"\:\"\[/g, "\":[")
+        .replace(/\]\"/g, "]")
+        .replace(/Object\(\{/g, "{")
+        .replace(/\}\)/g, "}");
 }
 
 function format(input) {
@@ -25,15 +38,30 @@ function format(input) {
         return input;
     }
 
-    var obj = JSON.parse(input);
+    log("Format input: " + input);
+    log("");
+
+    var obj = strategy === "parse" ? JSON.parse(input) : eval(input);
     return stringify(obj, { space: 4 });
 }
 
 var content = fs.readFileSync(file, 'utf8');
 
 var i = content.indexOf(split);
-var first = format(clean(content.substring(0, i)));
-var second = format(clean(content.substring(i + split.length)));
+
+var firstRaw = content.substring(0, i);
+var secondRaw = content.substring(i + split.length);
+
+log("first input raw: ");
+log(firstRaw);
+log("");
+log("===========================");
+log("");
+log("second input raw: ");
+log(secondRaw);
+
+var first = format(clean(firstRaw));
+var second = format(clean(secondRaw));
 
 var diff = jsdiff.diffLines(first, second);
 
